65a21ecdbe2ad6f696c5927fc00a64b2f747578c
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-05-29 03:45:04 +0530

    Day-13 and Day 14

---
+++ js/Day14_28_may/script.js
@@ -0,0 +1,59 @@
+var age = 24;// Number
+var str = "Utkarsh"; // String
+var isGoodBoy = true; // boolean
+var weight = 60.5; //Number
+var array = [1,2,3,4,5]; // Array
+console.log(typeof array) // object
+
+var x = 2;
+
+var y = 4;
+
+y = 6;
+
+
+var z = x+y;
+
+
+
+
+let s = "asstd"
+let zy = true;
+let arr = [1,2,3,4,5];
+
+
+const d = 3;
+
+
+//  d = 4;// cannot re assign variable
+
+
+
+
+
+console.log(z)
+
+console.log(typeof weight)
+
+
+var myFirstName = "Utkarsh" // camel Case
+var FullName = "Utkarsh Gupta"// Pascal Case
+var my_last_name = "Gupta" // Snake Case
+
+
+var schoolName;
+
+
+console.log(typeof schoolName) // undefined
+
+
+var schoolName=null;
+
+
+console.log(typeof schoolName)
+
+
+
+let obj  = {
+    name:"Satyam"
+} // object
