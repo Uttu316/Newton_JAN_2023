945d3a3d95f064f0ab7fc6de5c1502db7636b6bf
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-06-20 23:37:39 +0530

    Day 28

--- js/Day28_20_jun/inheritance.js
+++ js/Day28_20_jun/inheritance.js
@@ -1,48 +1,43 @@
 // inheritance
 
-
-class Mother{
-    constructor(x){
-        this.voice='low';
-        this.skills=['Art',"Craft","intelligence"]
-    }
+class Mother {
+  constructor(x) {
+    this.voice = "low";
+    this.skills = ["Art", "Craft", "intelligence"];
+  }
 }
 
-class Father extends Mother{
-    constructor(x){
-        super(x)
-        this.skinColor = "brown";
-        this.eyeColor = "black";
-        this.language = "hindi";
-        this.lastname = "gupta"
-    }
-    getEye(){
-      return this.eyeColor
-    }
-    speak(){
-        console.log(`I speak ${this.language}`)
-    }
+class Father extends Mother {
+  constructor(x) {
+    super(x);
+    this.skinColor = "brown";
+    this.eyeColor = "black";
+    this.language = "hindi";
+    this.lastname = "gupta";
+  }
+  getEye() {
+    return this.eyeColor;
+  }
+  speak() {
+    console.log(`I speak ${this.language}`);
+  }
 }
 
-class Child extends Father{
-    constructor(x){
-        super(x)
-        this.firstname = "Utkarsh";
-        this.age = 24;
-       
-    }
-    
+class Child extends Father {
+  constructor(x) {
+    super(x);
+    this.firstname = "Utkarsh";
+    this.age = 24;
+  }
 
-        customEye(){
-          
-                 let eye = super.getEye();
-            console.log(`My eye color is ${eye}`)
-        }
-    
+  customEye() {
+    let eye = super.getEye();
+    console.log(`My eye color is ${eye}`);
+  }
 }
 
-const c = new Child('abc')
+const c = new Child("abc");
 
-console.log(c)
-c.speak()
-c.customEye()
+console.log(c);
+c.speak();
+c.customEye();
